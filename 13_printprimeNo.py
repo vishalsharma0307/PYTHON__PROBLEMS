5e726b833cc5feae973eb3af23cc5d05cfab13c6
vishalsharma0307 <vish07sharma24@gmail.com>
2025-10-18 11:31:04 +0530

    error resolved

--- 13_printprimeNo.py
+++ 13_printprimeNo.py
@@ -1,23 +1,12 @@
 lower=int(input("enter lower no:"))
 upper=int(input("enter second no :"))
-if(lower==1):
-    print("not a prime")
-if(lower>1):
-    for el in range(2,lower):
-        if(lower%el==0):
-            print("is not a prime ")
-            break
-    else:
-        print("Is a prime  :",lower)
-num=lower
-if(num<=upper+1):
-    for num in range(lower,upper+1):
-            for i in range(2,upper+1):
-                if(num%i==0):
-                    print("Not a prime ")
-                    break
-    else:
-         print("IS prime,",num)     
+for num in range(lower,upper+1):
+    if (num>1):
+        for i in range(2,num):
+            if (num%i==0):
+                break
+        else:
+            print(num)
 
 
 
