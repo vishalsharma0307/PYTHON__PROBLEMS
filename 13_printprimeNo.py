eae6f4a1f5df789158fa9fa5dc64ed8eb7550dfc
vishalsharma0307 <vish07sharma24@gmail.com>
2025-10-18 12:35:01 +0530

    comment

--- 13_printprimeNo.py
+++ 13_printprimeNo.py
@@ -5,7 +5,7 @@ for num in range(lower,upper+1):
         for i in range(2,num):
             if (num%i==0):
                 break
-        else:
+        else:             #"else" is used to do some sort of work at the end of the for loop
             print(num)
 
 
