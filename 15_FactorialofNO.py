7e8caf2f7f172e8249d064e31595913334ba0ea4
vishalsharma0307 <vish07sharma24@gmail.com>
2025-10-18 12:59:08 +0530

    by recursion

--- 15_FactorialofNO.py
+++ 15_FactorialofNO.py
@@ -14,3 +14,13 @@
 #         print("the factorial of num is :",fact)
 
 #sol 2 by recursion
+
+def fact(a):
+    if(a==0):
+        return 1
+    else:
+        return (a * fact(a-1))
+num=int(input("enter a number :"))
+result=fact(num)
+print("the factorial is : ,",result)
+
