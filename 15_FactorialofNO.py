8bf6f174707af3a5e6bdd7dbe117e798ee3e2870
vishalsharma0307 <vish07sharma24@gmail.com>
2025-10-18 12:54:54 +0530

    by loops

---
+++ 15_FactorialofNO.py
@@ -0,0 +1,16 @@
+
+#sol 1 by loops
+
+# num= int(input("enter a number:"))
+# fact=1
+# if(num<0):
+#     print("factorial is not defined")
+# if(num==0):
+#     print("factorial of 0 is 1")
+# if(num>0):
+#     for i in range(1,num+1):
+#         fact=fact*i
+#     else:
+#         print("the factorial of num is :",fact)
+
+#sol 2 by recursion
