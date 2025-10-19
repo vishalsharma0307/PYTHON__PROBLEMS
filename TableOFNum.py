e4b5ee931aa9c0030d86ca2d321c2fd2f3522df7
vishalsharma0307 <vish07sharma24@gmail.com>
2025-10-19 09:21:15 +0530

    by while loop

--- TableOFNum.py
+++ TableOFNum.py
@@ -1,3 +1,13 @@
+# SOL 1 by for loop
+
+# Table_Of=int(input("Enter a Number :"))
+# for i in range(1,11):
+#     print(Table_Of,"*",i,"=",Table_Of*i)
+
+#sol 2 by while loop
+
 Table_Of=int(input("Enter a Number :"))
-for i in range(1,11):
+i=1
+while(i<=10):
     print(Table_Of,"*",i,"=",Table_Of*i)
+    i+=1
