4f7f7353b0e4b2494958420abc504a0d0f2d831c
vishalsharma0307 <vish07sharma24@gmail.com>
2025-10-21 14:47:49 +0530

    Hcf or Gcd solved

--- 25_FIndHCF.py
+++ 25_FIndHCF.py
@@ -1,8 +1,20 @@
 def hcf(x,y):
     if(x>y):
         small=y
+        return y
     else:
         small=x
-for i in range(1,small+1):
-    if((x%i==0)and(y%i==0)):
-        
+        return x
+a=int(input("Enter first NO:"))
+b=int(input("Enter Second NO:"))
+c=hcf(a,b)
+print(c)
+
+for i in range(1,c+1):
+    if((a%i==0)and(b%i==0)):
+        gcd=i
+else:
+    print(gcd,"Is the gcd or hcf ")
+
+
+
