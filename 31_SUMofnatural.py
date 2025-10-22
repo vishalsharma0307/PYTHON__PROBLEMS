c9f7bc064e68c279e4ccb1fa3f4bb7819793d396
vishalsharma0307 <vish07sharma24@gmail.com>
2025-10-22 12:22:01 +0530

    complete

--- 31_SUMofnatural.py
+++ 31_SUMofnatural.py
@@ -1,10 +1,12 @@
-def natural(n):
-    if(n==1):
-        return 1
+
+while(True):
+    def natural(n):
+        if(n==1):
+            return 1
+        else:
+            return n+natural(n-1)
+    x=int(input("enter a number :"))
+    if(x==0):
+        print("Enter positive NO:")
     else:
-        return n+natural(n-1)
-x=int(input("enter a number :"))
-if(x==0):
-    print("Enter positive NO:")
-else:
-    print("The sum is : ",natural(x))
+        print("The sum is : ",natural(x))
