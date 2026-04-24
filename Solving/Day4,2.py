2ac18f9ac211fef22c4c7337bf4a190d8f84cd4f
vishalsharma0307 <vish07sharma24@gmail.com>
2026-04-24 12:12:17 +0530

    frequency

---
+++ Solving/Day4,2.py
@@ -0,0 +1,10 @@
+# 10. Count frequency of a given character in a string.
+st='vishal sharma om prakash sharma'
+target='a'
+count=0
+for el in st:
+    if ( el=='a'):
+        count+=1
+
+print(count,'no of apperance')
+
